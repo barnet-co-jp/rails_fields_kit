73e7eb93b765a531e9a6f7c2159d3599e747635f
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-18 03:41:40 +0900

    feat: add token search table filter factory

--- lib/rails_fields_kit/table_filter_input.rb
+++ lib/rails_fields_kit/table_filter_input.rb
@@ -4,6 +4,12 @@ module RailsFieldsKit
   class TableFilterInput
     attr_reader :field_type, :field_name, :options
 
+    def self.token_search(field_name = :query, url: nil, **options)
+      filter_options = options.dup
+      filter_options[:url] = url if url
+      new(:token_search, field_name, **filter_options)
+    end
+
     def initialize(field_type = nil, field_name = nil, type: nil, **options)
       @field_type = (field_type || type || :combobox).to_sym
       @field_name = field_name&.to_s
