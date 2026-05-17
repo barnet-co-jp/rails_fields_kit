b6953e5b48ee71787e83f9a6271bb14ba64f86fe
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 23:46:26 +0900

    Add install generator load coverage

---
+++ spec/rails_fields_kit/install_generator_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "generators/rails_fields_kit/install_generator"
+
+RSpec.describe RailsFieldsKit::Generators::InstallGenerator do
+  it "loads the install generator" do
+    expect(described_class).to be < Rails::Generators::Base
+  end
+
+  it "uses the packaged templates directory as its source root" do
+    source_paths = described_class.source_paths.map(&:to_s)
+
+    expect(source_paths).to include(a_string_ending_with("lib/generators/rails_fields_kit/templates"))
+  end
+
+  it "ships the initializer and setup note templates" do
+    source_root = described_class.source_paths.first
+
+    expect(File.file?(File.join(source_root, "rails_fields_kit.rb"))).to be(true)
+    expect(File.file?(File.join(source_root, "rails_fields_kit_setup.md"))).to be(true)
+  end
+end
