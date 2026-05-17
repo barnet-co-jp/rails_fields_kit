26c5e26708a0ef23b1d8e308818d8a64ae25dbd8
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 23:48:25 +0900

    Fix install generator source root spec

--- spec/rails_fields_kit/install_generator_spec.rb
+++ spec/rails_fields_kit/install_generator_spec.rb
@@ -9,13 +9,11 @@ RSpec.describe RailsFieldsKit::Generators::InstallGenerator do
   end
 
   it "uses the packaged templates directory as its source root" do
-    source_paths = described_class.source_paths.map(&:to_s)
-
-    expect(source_paths).to include(a_string_ending_with("lib/generators/rails_fields_kit/templates"))
+    expect(described_class.source_root.to_s).to end_with("lib/generators/rails_fields_kit/templates")
   end
 
   it "ships the initializer and setup note templates" do
-    source_root = described_class.source_paths.first
+    source_root = described_class.source_root
 
     expect(File.file?(File.join(source_root, "rails_fields_kit.rb"))).to be(true)
     expect(File.file?(File.join(source_root, "rails_fields_kit_setup.md"))).to be(true)
