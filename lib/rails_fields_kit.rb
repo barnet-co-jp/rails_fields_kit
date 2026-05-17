e7629b9f33459dd73dee2acd841c5f8e81211993
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 18:29:48 +0900

    feat: expose configuration API

--- lib/rails_fields_kit.rb
+++ lib/rails_fields_kit.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 require "rails_fields_kit/version"
+require "rails_fields_kit/configuration"
 require "rails_fields_kit/engine"
 
 module RailsFieldsKit
   class Error < StandardError; end
+
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+
+    def reset_configuration!
+      @configuration = Configuration.new
+    end
+  end
 end
