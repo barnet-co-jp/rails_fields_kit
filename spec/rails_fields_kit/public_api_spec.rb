dacff68f7eac57c7ac70978315a8baa3eccf88e5
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-17 23:54:32 +0900

    Add public API coverage

---
+++ spec/rails_fields_kit/public_api_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "public API" do
+  it "exposes version and configuration APIs" do
+    expect(RailsFieldsKit::VERSION).to match(/\A\d+\.\d+\.\d+/)
+    expect(RailsFieldsKit).to respond_to(:configuration)
+    expect(RailsFieldsKit).to respond_to(:configure)
+    expect(RailsFieldsKit).to respond_to(:reset_configuration!)
+  end
+
+  it "exposes FormBuilder helpers" do
+    expect(RailsFieldsKit::FormBuilder.instance_methods).to include(
+      :rfk_select,
+      :rfk_combobox,
+      :rfk_autocomplete,
+      :rfk_tags,
+      :rfk_multi_select,
+      :rfk_grouped_select,
+      :rfk_enum_select,
+      :rfk_text_field,
+      :rfk_text_area,
+      :rfk_number_field,
+      :rfk_money_field,
+      :rfk_percent_field,
+      :rfk_email_field,
+      :rfk_url_field,
+      :rfk_phone_field,
+      :rfk_search_field
+    )
+  end
+
+  it "exposes Searchable controller helpers" do
+    expect(RailsFieldsKit::Searchable::ClassMethods.instance_methods).to include(
+      :rfk_search_with,
+      :rfk_find_with,
+      :rfk_create_with
+    )
+  end
+end
