253bd072ab5ceb29e48cc3f3082bcee2447fe7c1
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-18 21:39:42 +0900

    test: expose dynamic table input factories

--- spec/rails_fields_kit/public_api_spec.rb
+++ spec/rails_fields_kit/public_api_spec.rb
@@ -49,6 +49,7 @@ RSpec.describe "public API" do
   end
 
   it "exposes table filter factories" do
+    expect(RailsFieldsKit::TableFilterInput).to respond_to(:from_type)
     expect(RailsFieldsKit::TableFilterInput).to respond_to(:combobox)
     expect(RailsFieldsKit::TableFilterInput).to respond_to(:select)
     expect(RailsFieldsKit::TableFilterInput).to respond_to(:tags)
@@ -57,6 +58,7 @@ RSpec.describe "public API" do
   end
 
   it "exposes table cell editor factories" do
+    expect(RailsFieldsKit::TableCellInput).to respond_to(:from_type)
     expect(RailsFieldsKit::TableCellInput).to respond_to(:combobox)
     expect(RailsFieldsKit::TableCellInput).to respond_to(:select)
     expect(RailsFieldsKit::TableCellInput).to respond_to(:tags)
