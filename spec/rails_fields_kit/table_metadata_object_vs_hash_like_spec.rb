89667bd3a55b6e8171fe3b2c3cd7ed401807a05e
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-20 01:55:31 +0900

    test: prefer object metadata readers before to_hash

---
+++ spec/rails_fields_kit/table_metadata_object_vs_hash_like_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsFieldsKit::TableMetadata do
+  class ObjectAndHashLikeColumn
+    attr_reader :to_hash_calls
+
+    def initialize(filter)
+      @filter = filter
+      @to_hash_calls = 0
+    end
+
+    def filter
+      @filter
+    end
+
+    def to_hash
+      @to_hash_calls += 1
+
+      {
+        filter: RailsFieldsKit::TableFilterInput.search_field(:unexpected)
+      }
+    end
+  end
+
+  it "prefers object metadata readers before hash-like normalization" do
+    expected_filter = RailsFieldsKit::TableFilterInput.token_search(
+      :query,
+      url: "/tokens.json"
+    )
+
+    column = ObjectAndHashLikeColumn.new(expected_filter)
+
+    expect(described_class.filters([column])).to eq([
+      {
+        type: "rails_fields_kit",
+        field_type: "token_search",
+        method: "query",
+        options: { url: "/tokens.json" }
+      }
+    ])
+
+    expect(column.to_hash_calls).to eq(0)
+  end
+end
