b8043e583c3dbec23fc6341d71f7333a23c67611
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-19 12:13:40 +0900

    fix: protect table cell input options

--- lib/rails_fields_kit/table_cell_input.rb
+++ lib/rails_fields_kit/table_cell_input.rb
@@ -24,7 +24,7 @@ module RailsFieldsKit
 
     KNOWN_FIELD_TYPES = COMMON_FIELD_TYPES.freeze
 
-    attr_reader :field_type, :field_name, :options
+    attr_reader :field_type, :field_name
 
     class << self
       COMMON_FIELD_TYPES.each do |field_type|
@@ -55,12 +55,16 @@ module RailsFieldsKit
       @options = options
     end
 
+    def options
+      @options.dup
+    end
+
     def to_table_cell_editor
       {
         type: "rails_fields_kit",
         field_type: field_type.to_s,
         method: field_name,
-        options: options.dup
+        options: options
       }.compact
     end
   end
