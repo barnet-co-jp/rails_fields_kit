f7922f26457f505404d111cf2c0e923ac862c3f7
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-21 00:13:26 +0900

    test: cover table metadata duplication contracts

---
+++ spec/rails_fields_kit/table_metadata_duplication_contract_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsFieldsKit::TableMetadata do
+  it "duplicates collected filter metadata hashes" do
+    column = {
+      filter: {
+        field_type: "token_search",
+        method: :query,
+        options: {
+          url: "/tokens.json"
+        }
+      }
+    }
+
+    filters = described_class.filters([column])
+
+    filters.first[:field_type] = "mutated"
+    filters.first[:options][:url] = "/mutated.json"
+
+    expect(column).to eq(
+      filter: {
+        field_type: "token_search",
+        method: :query,
+        options: {
+          url: "/tokens.json"
+        }
+      }
+    )
+  end
+
+  it "duplicates collected cell editor metadata hashes" do
+    column = {
+      editor: {
+        field_type: "enum_select",
+        method: :status,
+        options: {
+          include_blank: true
+        }
+      }
+    }
+
+    editors = described_class.cell_editors([column])
+
+    editors.first[:field_type] = "mutated"
+    editors.first[:options][:include_blank] = false
+
+    expect(column).to eq(
+      editor: {
+        field_type: "enum_select",
+        method: :status,
+        options: {
+          include_blank: true
+        }
+      }
+    )
+  end
+end
