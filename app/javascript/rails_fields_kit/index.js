ccb5888d8f887370446f86447f95a64d4b4c8793
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-28 18:11:36 +0900

    feat: export rendered remote search config reader

--- app/javascript/rails_fields_kit/index.js
+++ app/javascript/rails_fields_kit/index.js
@@ -1,5 +1,10 @@
 import TomSelectController from "./tom_select_controller"
 import { readRenderedCreateOnTheFlyConfig } from "./rendered_create_on_the_fly_config"
+import { readRenderedRemoteSearchConfig } from "./rendered_remote_search_config"
 
-export { TomSelectController, readRenderedCreateOnTheFlyConfig }
+export {
+  TomSelectController,
+  readRenderedCreateOnTheFlyConfig,
+  readRenderedRemoteSearchConfig
+}
 export default TomSelectController
