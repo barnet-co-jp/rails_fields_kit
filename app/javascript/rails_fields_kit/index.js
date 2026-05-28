13be12e1af10d190035d34b6386d117263267ae0
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-28 16:15:07 +0900

    feat: export rendered create-on-the-fly config reader

--- app/javascript/rails_fields_kit/index.js
+++ app/javascript/rails_fields_kit/index.js
@@ -1,4 +1,5 @@
 import TomSelectController from "./tom_select_controller"
+import { readRenderedCreateOnTheFlyConfig } from "./rendered_create_on_the_fly_config"
 
-export { TomSelectController }
+export { TomSelectController, readRenderedCreateOnTheFlyConfig }
 export default TomSelectController
