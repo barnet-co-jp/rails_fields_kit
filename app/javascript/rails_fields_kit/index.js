53cf254a2ad72f226ed5d912bf3d55bed22c2ef2
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-28 22:18:30 +0900

    feat: add rendered interaction config reader

--- app/javascript/rails_fields_kit/index.js
+++ app/javascript/rails_fields_kit/index.js
@@ -1,5 +1,6 @@
 import TomSelectController from "./tom_select_controller"
+import { readRenderedInteractionConfig } from "./rendered_interaction_config"
 import { readRenderedOptionPayloadMapping } from "./rendered_option_payload_mapping"
 
-export { TomSelectController, readRenderedOptionPayloadMapping }
+export { TomSelectController, readRenderedInteractionConfig, readRenderedOptionPayloadMapping }
 export default TomSelectController
