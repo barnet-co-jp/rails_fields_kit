212555d87ce887e805a8fd2ee29768dbd4f185e6
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-05-29 03:15:56 +0900

    feat: add rendered text override reader helper

--- app/javascript/rails_fields_kit/index.js
+++ app/javascript/rails_fields_kit/index.js
@@ -1,4 +1,5 @@
 import TomSelectController from "./tom_select_controller"
+import { readRenderedTextOverrides } from "./rendered_text_overrides"
 
-export { TomSelectController }
+export { TomSelectController, readRenderedTextOverrides }
 export default TomSelectController
