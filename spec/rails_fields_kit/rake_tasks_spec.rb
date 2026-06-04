d5074862d822bc5984eab44caf2742ea0bbea083
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-05 07:13:40 +0900

    chore: refresh setup doctor PR on current main

--- spec/rails_fields_kit/rake_tasks_spec.rb
+++ spec/rails_fields_kit/rake_tasks_spec.rb
@@ -5,6 +5,7 @@ require "rake"
 
 RSpec.describe "Rake tasks" do
   let(:rakefile_path) { File.expand_path("../../Rakefile", __dir__) }
+  let(:task_path) { File.expand_path("../../lib/tasks/rails_fields_kit.rake", __dir__) }
   let(:rakefile) { File.read(rakefile_path) }
   let(:rake) { Rake::Application.new }
 
@@ -12,6 +13,7 @@ RSpec.describe "Rake tasks" do
     original_application = Rake.application
     Rake.application = rake
     load rakefile_path
+    load task_path
     example.run
   ensure
     Rake.application = original_application
@@ -23,6 +25,19 @@ RSpec.describe "Rake tasks" do
 
   it "defines the spec task" do
     expect(rake.lookup("spec")).not_to be_nil
+  end
+
+  it "defines the setup doctor task" do
+    expect(rake.lookup("rails_fields_kit:doctor")).not_to be_nil
+  end
+
+  it "runs the setup doctor task" do
+    doctor = instance_double(RailsFieldsKit::SetupDoctor)
+
+    expect(RailsFieldsKit::SetupDoctor).to receive(:new).and_return(doctor)
+    expect(doctor).to receive(:run)
+
+    rake["rails_fields_kit:doctor"].invoke
   end
 
   it "loads Bundler gem tasks for build and release" do
