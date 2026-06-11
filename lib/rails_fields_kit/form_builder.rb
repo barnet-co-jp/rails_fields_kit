7a339f54f20667d4b66be9169c060b1fdbd97c2a
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-12 00:10:06 +0900

    feat: add range field native wrapper

--- lib/rails_fields_kit/form_builder.rb
+++ lib/rails_fields_kit/form_builder.rb
@@ -18,6 +18,10 @@ module RailsFieldsKit
       rfk_native_field(method, :number_field, **options)
     end
 
+    def rfk_range_field(method, **options)
+      rfk_native_field(method, :range_field, **options)
+    end
+
     def rfk_money_field(method, currency: nil, **options)
       options[:inputmode] ||= "decimal"
       options[:prefix] = currency if currency
@@ -401,7 +405,7 @@ module RailsFieldsKit
       selected_choices.map(&:second)
     end
 
-    def rfk_normalize_collection(collection, value_method:, label_method:, disabled: nil, option_html: {})
+    def rfk_normalize_collection(collection, value_method:, label_method:)
       disabled_values = Array(disabled).map(&:to_s)
 
       case collection
