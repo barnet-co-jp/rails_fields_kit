a6212c065b43591ac7bd7f479aaac9b0e5e29c73
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-12 05:14:19 +0900

    Fix grouped select disabled option handling

--- lib/rails_fields_kit/grouped_select_options.rb
+++ lib/rails_fields_kit/grouped_select_options.rb
@@ -5,47 +5,29 @@ module RailsFieldsKit
     def rfk_grouped_select(method, grouped_collection:, **options)
       grouped_disabled = options[:disabled] unless [true, false].include?(options[:disabled])
       options.delete(:disabled) unless [true, false].include?(options[:disabled])
-      option_html = options.delete(:option_html) || {}
-      value_method = options[:collection_value_method] || options[:value_method] || :id
-      label_method = options[:collection_label_method] || options[:label_method] || :to_s
+      options.delete(:option_html)
 
-      options[:grouped_collection] = grouped_collection.map do |group_label, items|
-        [
-          group_label,
-          rfk_normalize_collection(
-            items,
-            value_method: value_method,
-            label_method: label_method,
-            disabled: grouped_disabled,
-            option_html: option_html
-          )
-        ]
+      if grouped_disabled
+        options[:selected] = rfk_grouped_select_selected_options(method, options[:selected], grouped_disabled)
       end
+
+      options[:grouped_collection] = grouped_collection
       rfk_tom_select_field(method, :select, collection: nil, **options)
     end
 
     private
 
-    def rfk_normalize_collection(collection, value_method:, label_method:, disabled: nil, option_html: {})
+    def rfk_grouped_select_selected_options(method, selected, disabled)
       disabled_values = Array(disabled).map(&:to_s)
 
-      case collection
-      when nil
-        []
-      when Hash
-        collection.map { |label, value| rfk_choice_with_html(label, value, disabled_values: disabled_values, option_html: option_html) }
+      if selected.is_a?(Hash)
+        selected.merge(disabled: Array(selected[:disabled] || selected["disabled"]).map(&:to_s) | disabled_values)
       else
-        collection.map do |item|
-          if item.is_a?(Array) && item.size == 3 && item[2].is_a?(Hash)
-            item
-          elsif item.is_a?(Array) && item.size == 2
-            rfk_choice_with_html(item.first, item.second, disabled_values: disabled_values, option_html: option_html)
-          else
-            value = rfk_read_selected_value(item, value_method)
-            label = rfk_read_selected_label(item, label_method)
-            rfk_choice_with_html(label, value, disabled_values: disabled_values, option_html: option_html)
-          end
-        end
+        selected_value = selected
+        selected_value = object.public_send(method) if selected_value.nil? && object.respond_to?(method)
+        return {disabled: disabled_values} if selected_value.nil?
+
+        {selected: selected_value, disabled: disabled_values}
       end
     end
   end
