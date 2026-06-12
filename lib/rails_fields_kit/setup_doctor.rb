3dd8c30da7b62434d5c80acff32eb5be0ba19f2e
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-12 23:16:44 +0900

    Add contract helper importmap pins

--- lib/rails_fields_kit/setup_doctor.rb
+++ lib/rails_fields_kit/setup_doctor.rb
@@ -7,7 +7,9 @@ module RailsFieldsKit
   class SetupDoctor
     IMPORTMAP_PINS = {
       "rails_fields_kit" => "rails_fields_kit/index.js",
-      "rails_fields_kit/tom_select_controller" => "rails_fields_kit/tom_select_controller.js"
+      "rails_fields_kit/native_field_accessibility_contract" => "rails_fields_kit/native_field_accessibility_contract.js",
+      "rails_fields_kit/tom_select_controller" => "rails_fields_kit/tom_select_controller.js",
+      "rails_fields_kit/tom_select_text_override_contract" => "rails_fields_kit/tom_select_text_override_contract.js"
     }.freeze
 
     CSS_IMPORT_CANDIDATE_PATHS = [
