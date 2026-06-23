2e9ad619bfa6e96e6506c1c70a596793af65a9f7
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-06-24 06:10:00 +0900

    feat: expose enum select rendered kind

--- lib/rails_fields_kit/form_builder.rb
+++ lib/rails_fields_kit/form_builder.rb
@@ -73,7 +73,7 @@ module RailsFieldsKit
       explicit_enum = !enum.nil?
       enum_values = explicit_enum ? enum : object.class.public_send(method.to_s.pluralize)
       collection = enum_values.keys.map { |key| [rfk_enum_label(method, key, explicit: explicit_enum), key] }
-      rfk_tom_select_field(method, :select, collection: collection, **options)
+      rfk_tom_select_field(method, :enum_select, collection: collection, **options)
     end
 
     def rfk_combobox(method, collection: nil, **options)
