d337d9d8cd81eaa84af4f65eb0789d281e5f4106
matsuo-haruhito <126528297+matsuo-haruhito@users.noreply.github.com>
2026-07-26 05:17:52 +0900

    Add dependent query params form builder wiring

---
+++ lib/rails_fields_kit/form_builder_dependent_query_params.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "json"
+
+module RailsFieldsKit
+  module FormBuilderDependentQueryParams
+    private
+
+    def rfk_tom_select_field(method, field_kind, collection: nil, **options)
+      depends_on = options.delete(:depends_on)
+      clear_on_dependency_change = options.delete(:clear_on_dependency_change)
+
+      if depends_on || !clear_on_dependency_change.nil?
+        html_options = (options[:html] || {}).dup
+        data = rfk_dependent_query_params_data(html_options)
+        rfk_assign_data_value(data, :depends_on, depends_on)
+        rfk_assign_data_value(data, :clear_on_dependency_change, clear_on_dependency_change)
+        html_options[:data] = data
+        options[:html] = html_options
+      end
+
+      super(method, field_kind, collection: collection, **options)
+    end
+
+    def rfk_dependent_query_params_data(html_options)
+      symbol_data = html_options[:data]
+      string_data = html_options["data"]
+      data = {}
+      data.merge!(string_data) if string_data.is_a?(Hash)
+      data.merge!(symbol_data) if symbol_data.is_a?(Hash)
+      data
+    end
+  end
+
+  FormBuilder.prepend(FormBuilderDependentQueryParams)
+end
